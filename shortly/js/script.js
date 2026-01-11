230982934d051a563b000dd2c8ec82105ef87e22
DarshanOracle <153791933+DarshanOracle@users.noreply.github.com>
2026-01-11 15:39:16 +0530

    Tailwind CSS Website project example Shortly

---
+++ shortly/js/script.js
@@ -0,0 +1,56 @@
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu')
+
+const input = document.getElementById('link-input')
+const linkForm = document.getElementById('link-form')
+const errMsg = document.getElementById('err-msg')
+
+// Animated Hamburger Toggle
+const hamburger = document.getElementById('hamburger')
+if (hamburger) {
+  hamburger.addEventListener('click', () => {
+    const active = hamburger.classList.toggle('active');
+    hamburger.setAttribute('aria-expanded', active ? 'true' : 'false');
+    document.getElementById("mobile-menu").classList.toggle("hidden")
+  })
+}
+
+if (btn && menu) btn.addEventListener('click', navToggle)
+if (linkForm) linkForm.addEventListener('submit', formSubmit)
+
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i'
+  )
+  return !!pattern.test(str)
+}
+
+function formSubmit(e) {
+  e.preventDefault()
+
+  if (input.value === '') {
+    errMsg.innerHTML = 'Please enter something'
+    input.classList.add('border-red-500', 'text-red-500')
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = 'Please enter a valid URL'
+    input.classList.add('border-red-500', 'text-red-500')
+  } else {
+    errMsg.innerHTML = ''
+    input.classList.remove('border-red-500', 'text-red-500')
+    alert('Success')
+  }
+}
