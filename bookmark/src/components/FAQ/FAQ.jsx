a8c486bde5c51dba7c36c5357646a0f730ce581c
DarshanOracle <153791933+DarshanOracle@users.noreply.github.com>
2026-01-16 18:11:59 +0530

    Update Faq
    using JSON to dynamically load FAQs

--- bookmark/src/components/FAQ/FAQ.jsx
+++ bookmark/src/components/FAQ/FAQ.jsx
@@ -1,24 +1,24 @@
-import { useCallback, useId, useState } from "react";
+import { createRef, useCallback, useRef, useState } from "react";
 import "./FAQ.css";
+import FAQs from  "../../data/faq.json";
 
 function FAQ() {
-    const id1 = useId();
-    const id2 = useId();
-    const id3 = useId();
-    const id4 = useId();
+
+    const refs = useRef(FAQs.map(() => createRef()));
 
     // Using event targeting instead of refs for robust DOM access
 
     const [activeAns, setActiveAns] = useState();
 
-    const showFAQ = useCallback((event) => {
-        event.preventDefault();
+    const showFAQ = useCallback((index) => {
+        // event.preventDefault();
 
         // Find the closest container that represents the FAQ item
-        const item = event.currentTarget.closest('[data-faq-item]');
-        if (!item) return;
+        const activeRef = refs.current[index];
 
-        if (activeAns && activeAns.id === item.id) {
+        if (!activeRef) return;
+
+        if (activeAns && activeAns.id === activeRef.id) {
             activeAns.classList.remove("is-active");
             setActiveAns(null);
             return;
@@ -28,8 +28,8 @@ function FAQ() {
             activeAns.classList.remove("is-active");
         }
 
-        item.classList.add("is-active");
-        setActiveAns(item);
+        activeRef.classList.add("is-active");
+        setActiveAns(activeRef);
     }, [activeAns]);
 
     return(
@@ -39,50 +39,19 @@ function FAQ() {
                     <h2 className="text-5xl font-display font-semibold text-center">Frequently Asked Questions</h2>
                     <p className="text-lg text-gray-500 text-center">Here are some of our FAQs. If you have any other questions you'd like answered please feel free to email us.</p>
                     <div className="flex flex-col items-center space-y-4 w-full lg:w-3/5 mx-auto faq-list">
-                        <div data-faq-item id={id1} className={"flex flex-col justify-between w-full border-b border-b-gray-200 pb-3 px-6"}>
-                            <button type="button" onClick={showFAQ} className="inline-flex justify-between items-center w-full">
-                                <h5 className="text-lg">What is Bookmark?</h5>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="chevron">
-                                    <path fill="none" stroke="currentColor" strokeWidth="3" d="M1 1l8 8 8-8"></path>
-                                </svg>
-                            </button>
-                            <div className="answer text-gray-400 ">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima velit recusandae blanditiis laudantium harum qui odit accusantium culpa quibusdam nesciunt in autem, voluptas a aspernatur et sequi, architecto quam asperiores?
+                        { FAQs.map((faq, index) => (
+                            <div key={index} id={`faq-id-${index}`} data-faq-item ref={(ref) => refs.current[index] = ref} className={"flex flex-col justify-between w-full border-b border-b-gray-200 pb-3 px-6"}>
+                                <button type="button" onClick={() => showFAQ(index)} className="inline-flex justify-between items-center w-full">
+                                    <h5 className="text-lg">{ faq.question }</h5>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="chevron">
+                                        <path fill="none" stroke="currentColor" strokeWidth="3" d="M1 1l8 8 8-8"></path>
+                                    </svg>
+                                </button>
+                                <div className="answer text-gray-400 ">
+                                    { faq.answer }
+                                </div>
                             </div>
-                        </div>
-                        <div data-faq-item id={id2} className={"flex flex-col justify-between w-full  border-b border-b-gray-200 pb-3 px-6"}>
-                            <button type="button" onClick={showFAQ} className="inline-flex justify-between items-center w-full">
-                                <h5 className="text-lg">How can I request a new Browser?</h5>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="chevron">
-                                    <path fill="none" stroke="currentColor" strokeWidth="3" d="M1 1l8 8 8-8"></path>
-                                </svg>
-                            </button>
-                            <div className="answer text-gray-400">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima velit recusandae blanditiis laudantium harum qui odit accusantium culpa quibusdam nesciunt in autem, voluptas a aspernatur et sequi, architecto quam asperiores?
-                            </div>
-                        </div>
-                        <div data-faq-item id={id3} className={"flex flex-col justify-between w-full  border-b border-b-gray-200 pb-3 px-6"}>
-                            <button type="button" onClick={showFAQ} className="inline-flex justify-between items-center w-full">
-                                <h5 className="text-lg">Is there a mobile app?</h5>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="chevron">
-                                    <path fill="none" stroke="currentColor" strokeWidth="3" d="M1 1l8 8 8-8"></path>
-                                </svg>
-                            </button>
-                            <div className="answer text-gray-400">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima velit recusandae blanditiis laudantium harum qui odit accusantium culpa quibusdam nesciunt in autem, voluptas a aspernatur et sequi, architecto quam asperiores?
-                            </div>
-                        </div>
-                        <div data-faq-item id={id4} className={"flex flex-col justify-between w-full  border-b border-b-gray-200 pb-3 px-6"}>
-                            <button type="button" onClick={showFAQ} className="inline-flex justify-between items-center w-full">
-                                <h5 className="text-lg">What about other Chromium browsers?</h5>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12" className="chevron">
-                                    <path fill="none" stroke="currentColor" strokeWidth="3" d="M1 1l8 8 8-8"></path>
-                                </svg>
-                            </button>
-                            <div className="answer text-gray-400">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima velit recusandae blanditiis laudantium harum qui odit accusantium culpa quibusdam nesciunt in autem, voluptas a aspernatur et sequi, architecto quam asperiores?
-                            </div>
-                        </div>
+                        )) }
                     </div>
                 </div>
             </div>
